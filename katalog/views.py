a7e013c6f70e1b98f753cd36a3bf2f7ce4d6e01f
aliccciao <alicia.utomo@gmail.com>
2022-09-14 10:18:17 +0700

    setup views

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    return render(request, "katalog.html", context)
+
+data_katalog = CatalogItem.objects.all()
+context = {
+    'list_barang': data_katalog,
+    'nama': 'Alicia Kirana Utomo',
+    'id': '2106751505'
+}
